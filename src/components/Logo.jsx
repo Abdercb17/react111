e38e54a550a95865b2322fca1907058157e7cffd
Abdercb17 <kishan9535199605@gmail.com>
2024-10-08 23:10:26 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/kk.png";
 export default function Logo() {
   return (
     <div className="w-[100px]">
